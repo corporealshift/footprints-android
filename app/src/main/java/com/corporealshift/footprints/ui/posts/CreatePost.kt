b544989c3706c449b8ea73c3f1e39b2d4028b177
Kyle Diedrick <corporealshift@gmail.com>
2023-01-24 19:09:40 -0500

    Add title field to create page

--- app/src/main/java/com/corporealshift/footprints/ui/posts/CreatePost.kt
+++ app/src/main/java/com/corporealshift/footprints/ui/posts/CreatePost.kt
@@ -1,8 +1,17 @@
 package com.corporealshift.footprints.ui.posts
 
 import android.content.Context
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.corporealshift.footprints.ui.timelines.TimelineModel
 import org.chromium.net.CronetEngine
@@ -15,5 +24,22 @@ fun CreatePostScreen(
     engine: CronetEngine,
     executor: Executor,
 ) {
-
+    // Parts of the create screen:
+    var title by remember { mutableStateOf("") }
+    Column (Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        // title
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = title,
+            onValueChange = { title = it },
+            label = { Text("Title") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next
+            ),
+        )
+    }
+    // body
+    // media items - media selector?
+    // Photos are uploaded separately, then added to posts
 }
